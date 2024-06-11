892419c65e9afed2e9439d505c8dcf20f19e1ace
Artem Kalachev <lunycat@mail.ru>
2024-06-11 21:46:08 +0300

    add checkstyle plugin

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Cli {
     public static void start() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Welcome to the Brain Games!\n" +
-                "May I have your name? ");
+        System.out.print("Welcome to the Brain Games!\n"
+                + "May I have your name? ");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
         sc.close();
